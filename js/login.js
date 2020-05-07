ee8ff10b98a1474c5694368b5c7b72317e1a5cdf
Cris Duran <cris@franssonduran.se>
2020-05-07 12:16:42 +0200

    Add login script and config file.

---
+++ js/login.js
@@ -0,0 +1,7 @@
+import config from './modules/config.js';
+
+const login = document.querySelector( '#login' );
+
+login.addEventListener( 'click', ()=> {
+    window.location = `${config.authURI}?client_id=${config.clientID}&response_type=${config.response}`; 
+});
---
+++ js/modules/config.js
@@ -0,0 +1,19 @@
+/**
+ * Config for OAuth2 and REST requests.
+ * 
+ * This will have to change in production.
+ * @TODO: Research how config files are managed live.
+ */
+
+ const rootURL = '';
+
+ const config = {
+     rootURL: rootURL,
+     taskRoute: `${rootURL}/wp-json/wp/v2/tasks/`,
+     authURI: `${rootURL}/oauth/authorize`,
+     clientID: '',
+     responseType: 'token',
+     tokenName: 'taskAppToken'
+ }
+
+ export default config;
