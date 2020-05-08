5f21045acd23ec8499ce4e1eac9b8029b00018e7
Cris Duran <cris@franssonduran.se>
2020-05-08 14:43:46 +0200

    Add the auth loop.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 import config from './modules/config.js';
+import token from './modules/auth.js';
+
+if ( token === null ) {
+    window.location = `${window.location.origin}/login.html`;
+    
+} else {
+    console.log( 'token: ', token );
+}
